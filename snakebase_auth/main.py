1c118cc49cfa764cc16d407fcc6fd6470884af36
Bartosz 'b4rti' Gorol <bartgorol@t-online.de>
2023-02-08 00:00:39 +0000

    Init

--- snakebase_auth/main.py
+++ snakebase_auth/main.py
@@ -8,62 +8,76 @@ engine = create_engine("sqlite:///db.sqlite")
 
 
 class Settings(BaseModel):
-  autoconfirm: bool = False
-  disable_signup: bool = False
-  external: dict[str,bool] = {}
+    autoconfirm: bool = False
+    disable_signup: bool = False
+    external: dict[str, bool] = {}
+
 
 @app.get("/settings")
 async def settings() -> Settings:
-  pass
+    return Settings()
+
 
 class Invite(BaseModel):
-  email: str
+    email: str
+
 
 @app.post("/invite")
 async def invite(data: Invite):
-  pass
+    pass
+
 
 class Signup(BaseModel):
-  email: str
-  password: str
+    email: str
+    password: str
+
 
 @app.post("/signup")
 async def signup(data: Signup):
-  pass
+    pass
+
 
 @app.post("/token")
-async def token(query: str | None):
-  pass
+async def token(grant_type: str, username: str | None = None, password: str | None = None, refresh_token: str | None = None, code: str | None = None):
+    pass
+
 
 class Recover(BaseModel):
-  email: str
+    email: str
+
 
 @app.post("/recover")
 async def recover(data: Recover):
-  pass
+    pass
+
 
 class Verify(BaseModel):
-  type: str
-  token: str
-  password: str | None = None
+    type: str
+    token: str
+    password: str | None = None
+
 
 @app.post("/verify")
 async def verify(data: Verify):
-  pass
+    pass
+
 
 @app.post("/logout")
 async def logout():
-  pass
+    pass
+
 
 @app.get("/user")
 async def get_user():
-  pass
+    pass
+
 
 class UpdateUser(BaseModel):
-  email: str | None = None
-  password: str | None = None
-  data: dict[str,Any] | None = None
+    email: str | None = None
+    password: str | None = None
+    data: dict | None = None
+
 
 @app.put("/user")
 async def update_user(data: UpdateUser):
-  pass
+    pass
